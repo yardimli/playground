8a8f716471303de38d7e38707d3f662672ead94d
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2024-09-26 22:22:22 +0800

    Update AddMissingBookFields.php

--- app/Console/Commands/AddMissingBookFields.php
+++ app/Console/Commands/AddMissingBookFields.php
@@ -40,36 +40,36 @@
 			$bookFiles = scanForBookPhp($booksDir);
 
 			$genres_array = array(
-			"Action",
-			"Biography",
-			"Body, Mind & Spirit",
-			"Business & Economics",
-			"Education",
-			"Family & Relationships",
-			"Health & Fitness",
-			"Romance",
-			"Young Adult",
-			"Horror",
-			"Fantasy",
-			"Realistic",
-			"LGBTQ+",
-			"Science Fiction",
-			"Dark Humor",
-			"Mystery",
-			"Thriller",
-			"Historical",
-			"Paranormal",
-			"Adventure",
-			"Crime",
-			"Children's Literature",
-			"Steampunk",
-			"Chick Lit",
-			"Post-Apocalyptic",
-			"Humor",
-			"Drama",
-			"Western",
-			"Space Opera"
-		);
+				"Action",
+				"Biography",
+				"Body, Mind & Spirit",
+				"Business & Economics",
+				"Education",
+				"Family & Relationships",
+				"Health & Fitness",
+				"Romance",
+				"Young Adult",
+				"Horror",
+				"Fantasy",
+				"Realistic",
+				"LGBTQ+",
+				"Science Fiction",
+				"Dark Humor",
+				"Mystery",
+				"Thriller",
+				"Historical",
+				"Paranormal",
+				"Adventure",
+				"Crime",
+				"Children's Literature",
+				"Steampunk",
+				"Chick Lit",
+				"Post-Apocalyptic",
+				"Humor",
+				"Drama",
+				"Western",
+				"Space Opera"
+			);
 
 
 			foreach ($bookFiles as $bookFile) {
@@ -79,12 +79,13 @@
 
 				// Add missing fields
 				if (!isset($data['genre'])) {
-					$prompt = "Choose a genre for the book with titile " . $data['title'] . " and back cover text: " . $data['back_cover_text'] . "\n" . "Choose from the following genres: ".implode(", ", $genres_array). " only output the genre name\n";
+					$prompt = "Choose a genre for the book with titile " . $data['title'] . " and back cover text: " . $data['back_cover_text'] . "\n" . "Choose from the following genres: " . implode(", ", $genres_array) . " only output the genre name\n";
 					$promptResult = MyHelper::llm_no_tool_call('open-ai-gpt-4o-mini', '', '', $prompt, false, 'english');
 					$this->info("Genre: " . $promptResult);
 
 					$data['genre'] = "Fiction"; // You might want to set this dynamically
 				}
+
 				if (!isset($data['tags'])) {
 					$prompt = "Respond with 10 comma separated keywords for a book with the title: " . $data['title'] . " and back cover text: " . $data['back_cover_text'] . "\n";
 					$promptResult = MyHelper::llm_no_tool_call('open-ai-gpt-4o-mini', '', '', $prompt, false, 'english');
@@ -92,6 +93,7 @@
 
 					$data['tags'] = explode(',', $promptResult);
 				}
+
 				if (!isset($data['author_name'])) {
 					$prompt = "Respond with a random author name for a book with the title: " . $data['title'] . " and back cover text: " . $data['back_cover_text'] . "\nOnly output the author name\n";
 					$promptResult = MyHelper::llm_no_tool_call('open-ai-gpt-4o-mini', '', '', $prompt, false, 'english');
@@ -99,12 +101,17 @@
 
 					$data['author_name'] = $promptResult;
 				}
+
 				if (!isset($data['publisher_name'])) {
 					$prompt = "Respond with a random publishing house name for a book with the title: " . $data['title'] . " and back cover text: " . $data['back_cover_text'] . "\nOnly output the publishing house name\n";
 					$promptResult = MyHelper::llm_no_tool_call('open-ai-gpt-4o-mini', '', '', $prompt, false, 'english');
 					$this->info("Publisher: " . $promptResult);
 
 					$data['publisher_name'] = $promptResult;
+				}
+
+				if (!isset($data['public-domain'])) {
+					$data['public-domain'] = 'yes';
 				}
 
 				// Save the updated content back to the file
