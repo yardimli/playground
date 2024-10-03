132b9afcc147098a1fc1b2680a3b2d620f0d105f
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2024-10-03 15:28:59 +0800

    Update edit-book.blade.php

--- resources/views/user/edit-book.blade.php
+++ resources/views/user/edit-book.blade.php
@@ -474,16 +474,16 @@
 
 <!-- Delete Confirmation Modal -->
 <div class="modal fade" id="deleteConfirmModal" tabindex="-1" aria-labelledby="deleteConfirmModalLabel" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content modal-content-color">
+			<div class="modal-header modal-header-color">
 				<h5 class="modal-title" id="deleteConfirmModalLabel">Confirm Delete</h5>
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
-			<div class="modal-body">
+			<div class="modal-body modal-body-color">
 				Are you sure you want to delete this book?
 			</div>
-			<div class="modal-footer">
+			<div class="modal-footer modal-footer-color">
 				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
 				<button type="button" class="btn btn-danger" id="confirmDeleteBtn">Delete</button>
 			</div>
