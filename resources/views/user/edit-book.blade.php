1ce76d7fa7a3da40615c9b8197e4e90e56c26007
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2024-10-24 12:13:01 +0800

    Update edit-book.blade.php

--- resources/views/user/edit-book.blade.php
+++ resources/views/user/edit-book.blade.php
@@ -5,6 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>{{__('default.Write Books With AI')}}</title>
+
+	
 	
 	<!-- FAVICON AND TOUCH ICONS -->
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
